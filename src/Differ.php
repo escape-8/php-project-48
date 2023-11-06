3e586541bf2c94726c0fa8ac5ca703f55db11146
Mikhail Tsepelev <cplvmhl@gmail.com>
2023-11-06 12:20:13 +0300

    feat: implement differ

---
+++ src/Differ.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Gendiff\Differ;
+
+function genDiff($pathToFile1, $pathToFile2, $format = 'stylish'): string
+{
+    $dataFile1 = json_decode(file_get_contents(__DIR__ . $pathToFile1), true);
+    $dataFile2 = json_decode(file_get_contents(__DIR__ . $pathToFile2), true);
+    ksort($dataFile1);
+    ksort($dataFile2);
+    $result = [];
+    foreach ($dataFile1 as $key => $valueFile1) {
+        if (is_bool($valueFile1)) {
+            $valueFile1 = var_export($valueFile1, true);
+        }
+        if (array_key_exists($key, $dataFile2)) {
+            $valueFile2 = $dataFile2[$key];
+
+            if (is_bool($valueFile2)) {
+                $valueFile2 = var_export($valueFile2, true);
+            }
+
+            if ($valueFile2 === $valueFile1) {
+                $result[] = '    ' . $key . ': ' . $valueFile1;
+            } else {
+                $result[] = '  - ' . $key . ': ' . $valueFile1;
+                $result[] = '  + ' . $key . ': ' . $valueFile2;
+            }
+        } else {
+            $result[] = '  - ' . $key . ': ' . $valueFile1;
+        }
+    }
+
+    foreach ($dataFile2 as $key => $valueFile2) {
+        if (is_bool($valueFile2)) {
+            $valueFile2 = var_export($valueFile2, true);
+        }
+        if (!array_key_exists($key, $dataFile1)) {
+            $result[] = '  + ' . $key . ': ' . $valueFile2;
+        }
+    }
+
+    return "{\n" . implode("\n", $result) . "\n}\n";
+}
