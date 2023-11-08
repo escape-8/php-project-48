c6716adf91ed02ce7d53381a4d08db1c57eee822
Mikhail Tsepelev <cplvmhl@gmail.com>
2023-11-08 14:19:50 +0300

    feat: add new files to tests

---
+++ tests/GendiffTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Hexlet\Code\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Gendiff\Differ\genDiff;
+
+class GendiffTest extends TestCase
+{
+    public function createFixturePath(string $fixtureName): string
+    {
+        $parts = ['tests', 'fixtures', $fixtureName];
+        return (string) realpath(implode('/', $parts));
+    }
+
+    public function testPlainFiles1(): void
+    {
+        $expected = file_get_contents($this->createFixturePath('testPlainDifferResult1.txt'));
+        $pathFile1 = $this->createFixturePath('file1.json');
+        $pathFile2 = $this->createFixturePath('file2.json');
+        $this->assertEquals($expected, genDiff($pathFile1, $pathFile2));
+    }
+
+    public function testPlainFiles2(): void
+    {
+        $expected = file_get_contents($this->createFixturePath('testPlainDifferResult2.txt'));
+        $pathFile1 = $this->createFixturePath('file3.json');
+        $pathFile2 = $this->createFixturePath('file4.json');
+        $this->assertEquals($expected, genDiff($pathFile1, $pathFile2));
+    }
+
+    public function testPlainFiles3(): void
+    {
+        $expected = file_get_contents($this->createFixturePath('testPlainDifferResult1.txt'));
+        $pathFile1 = $this->createFixturePath('file1.yml');
+        $pathFile2 = $this->createFixturePath('file2.yaml');
+        $this->assertEquals($expected, genDiff($pathFile1, $pathFile2));
+    }
+
+    public function testPlainFiles4(): void
+    {
+        $expected = file_get_contents($this->createFixturePath('testPlainDifferResult2.txt'));
+        $pathFile1 = $this->createFixturePath('file3.yml');
+        $pathFile2 = $this->createFixturePath('file4.yaml');
+        $this->assertEquals($expected, genDiff($pathFile1, $pathFile2));
+    }
+}
