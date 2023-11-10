42b685798bc872a790cd4d52bf1ab1df1c47393a
Mikhail Tsepelev <cplvmhl@gmail.com>
2023-11-10 10:45:44 +0300

    feat: add recursive diff function

--- src/Differ.php
+++ src/Differ.php
@@ -3,44 +3,83 @@
 namespace Gendiff\Differ;
 
 use function Gendiff\Parsers\parseToData;
+use function Gendiff\Formatters\createFormat;
 
 function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
 {
     $dataFile1 = parseToData($pathToFile1);
     $dataFile2 = parseToData($pathToFile2);
-    ksort($dataFile1);
-    ksort($dataFile2);
-    $result = [];
-    foreach ($dataFile1 as $key => $valueFile1) {
-        if (is_bool($valueFile1)) {
-            $valueFile1 = var_export($valueFile1, true);
-        }
-        if (array_key_exists($key, $dataFile2)) {
-            $valueFile2 = $dataFile2[$key];
+    $diff = createDiff($dataFile1, $dataFile2);
+    return createFormat($diff, $format);
+}
 
-            if (is_bool($valueFile2)) {
-                $valueFile2 = var_export($valueFile2, true);
+function createDiff(array $dataFile1, array $dataFile2): array
+{
+    $dataSum = array_merge($dataFile1, $dataFile2);
+    ksort($dataSum);
+    $dataSumKeys = array_keys($dataSum);
+
+    $diff = array_map(function ($key, $valueFileSum) use ($dataFile2, $dataFile1) {
+        if (is_array($valueFileSum)) {
+            if (array_key_exists($key, $dataFile1) && array_key_exists($key, $dataFile2)) {
+                $valueFile1 = checkNull($dataFile1[$key]);
+                $valueFile2 = checkNull($dataFile2[$key]);
+                if (is_array($valueFile1) && is_array($valueFile2)) {
+                    return ['  ' . $key => createDiff($valueFile1, $valueFile2)];
+                }
+                if (is_array($valueFile1)) {
+                    return ['  ' . $key => createDiff($valueFile1, [$valueFile2])];
+                }
+
+                return ['  ' . $key => createDiff([$valueFile1], $valueFile2)];
+            }
+        }
+
+        if (array_key_exists($key, $dataFile1) && array_key_exists($key, $dataFile2)) {
+            $valueFile1 = checkNull($dataFile1[$key]);
+            $valueFile2 = checkNull($dataFile2[$key]);
+
+            if ($valueFile1 === $valueFile2) {
+                return ['  ' . $key => $valueFile1];
+            }
+            if (is_array($valueFile1)) {
+                return array_merge(
+                    [],
+                    ['- ' . $key => createDiff($valueFile1, $valueFile1)],
+                    ['+ ' . $key => $valueFile2]
+                );
             }
 
-            if ($valueFile2 === $valueFile1) {
-                $result[] = '    ' . $key . ': ' . $valueFile1;
-            } else {
-                $result[] = '  - ' . $key . ': ' . $valueFile1;
-                $result[] = '  + ' . $key . ': ' . $valueFile2;
+            if (is_array($valueFile2)) {
+                return array_merge(
+                    [],
+                    ['- ' . $key => $valueFile1],
+                    ['+ ' . $key => createDiff($valueFile2, $valueFile2)]
+                );
             }
-        } else {
-            $result[] = '  - ' . $key . ': ' . $valueFile1;
-        }
-    }
 
-    foreach ($dataFile2 as $key => $valueFile2) {
-        if (is_bool($valueFile2)) {
-            $valueFile2 = var_export($valueFile2, true);
+            return array_merge([], ['- ' . $key => $valueFile1], ['+ ' . $key => $valueFile2]);
         }
-        if (!array_key_exists($key, $dataFile1)) {
-            $result[] = '  + ' . $key . ': ' . $valueFile2;
-        }
-    }
 
-    return "{\n" . implode("\n", $result) . "\n}\n";
+        if (array_key_exists($key, $dataFile1)) {
+            $valueFile1 = $dataFile1[$key];
+            if (is_array($valueFile1)) {
+                return ['- ' . $key => createDiff($valueFile1, $valueFile1)];
+            }
+            return ['- ' . $key => $valueFile1];
+        }
+
+        $valueFile2 = $dataFile2[$key];
+        if (is_array($valueFile2)) {
+            return ['+ ' . $key => createDiff($valueFile2, $valueFile2)];
+        }
+        return ['+ ' . $key => $valueFile2];
+    }, $dataSumKeys, $dataSum);
+
+    return array_merge([], ...$diff);
+}
+
+function checkNull(mixed $item): mixed
+{
+    return is_null($item) ? 'null' : $item;
 }
