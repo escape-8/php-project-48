acf73918092cc2b33984f9f95e29013877dcff49
Mikhail Tsepelev <cplvmhl@gmail.com>
2023-11-11 14:36:42 +0300

    feat: add new view format

--- src/Formatters.php
+++ src/Formatters.php
@@ -4,11 +4,17 @@ namespace Gendiff\Formatters;
 
 use function Gendiff\Formatters\Stylish\stringify;
 use function Gendiff\Formatters\Plain\plainStringify;
+use function Gendiff\Formatters\Json\jsonStringify;
 
 function createFormat(array $data, string $format): string
 {
     if ($format === 'plain') {
         return plainStringify($data);
     }
+
+    if ($format === 'json') {
+        return jsonStringify($data);
+    }
+
     return stringify($data);
 }
